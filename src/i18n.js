7ffef6ab3caafcae9c6e89268899bd3707fb513f
taovc <tao.weijie@epitech.eu>
2023-02-24 16:44:44 +0100

    [fix] achitectural changes

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import homeCh from "./lang/Home/lang";
+import homeCh from "./lang/Home";
 
 const resources = {
   ch: {
