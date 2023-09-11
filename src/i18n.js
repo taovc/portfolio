5c24aaa22200ddd67116761e81f39bdf8e45553e
taovc <tao.weijie@epitech.eu>
2023-09-11 10:53:03 +0200

    fix: sitemap and data files

--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import homeCh from "./lang/Home";
-import aboutCh from "./lang/About";
-import projectCh from "./lang/Project";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
-
-const resources = {
-  CH: {
-    translation: {
-      ...homeCh,
-
-      About: "关于",
-      ME: "我",
-      Home: "主页",
-      Projects: "项目",
-      CV: "简历",
-
-      ...aboutCh,
-
-      ...projectCh,
-    },
-  },
-};
 
 i18n
   .use(Backend)
