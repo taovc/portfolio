510312c746e76f631254c0ace41653c15841e319
taovc <tao.weijie@epitech.eu>
2023-09-16 15:30:03 +0200

    feat: add config files adn run yarn eject ```

--- src/components/Home/About/About.js
+++ src/components/Home/About/About.js
@@ -4,7 +4,7 @@ import Particle from "../../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "@/src/Assets/about.png";
+import laptopImg from "@/Assets/about.png";
 import Toolstack from "./Toolstack";
 import { useTranslation } from "react-i18next";
 import ContactIcons from "../ContactIcons";
