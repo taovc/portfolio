9f6d6a7c8d54a6bd7e604bae0d766d5caf7d2a80
taovc <tao.weijie@epitech.eu>
2023-09-19 16:46:34 +0200

    perf: opti code for project card

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,15 +5,37 @@ import { CgWebsite, CgMail } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  const buttonInfo = [
+    {
+      icon: <BsGithub />,
+      label: props.isBlog ? "Blog" : "GitHub",
+      link: props.ghLink,
+      isHidden: !props.ghLink,
+    },
+    {
+      icon: <CgMail />,
+      label: "Contact",
+      link: "mailto:vincentao2002@gmail.com",
+      isHidden: !props.isMail,
+    },
+    {
+      icon: <CgWebsite />,
+      label: "Demo",
+      link: props.demoLink,
+      isHidden: !props.demoLink && !props.isBlog,
+    },
+    {
+      icon: <CgWebsite />,
+      label: "Web Site",
+      link: props.webLink,
+      isHidden: !props.webLink,
+    },
+  ];
+
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body
-        style={{
-          paddingBottom: "50px",
-          position: "relative",
-        }}
-      >
+      <Card.Body style={{ paddingBottom: "50px", position: "relative" }}>
         <Card.Title className="purple_text">{props.title}</Card.Title>
         <br></br>
         <Card.Text className="project-card-text">{props.description}</Card.Text>
@@ -26,51 +48,25 @@ function ProjectCards(props) {
             bottom: "15px",
           }}
         >
-          <Button
-            variant="primary"
-            hidden={!props.ghLink}
-            href={props.ghLink}
-            target="_blank"
-          >
-            <BsGithub /> &nbsp;
-            {props.isBlog ? "Blog" : "GitHub"}
-          </Button>
-
-          <Button
-            variant="primary"
-            hidden={!props.isMail}
-            target="_blank"
-            href="mailto:vincentao2002@gmail.com"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgMail /> &nbsp;
-            {"Contact"}
-          </Button>
-
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            hidden={!props.demoLink && !props.isBlog}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-
-          <Button
-            variant="primary"
-            href={props.webLink}
-            hidden={!props.webLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Web Site"}
-          </Button>
+          {buttonInfo.map(
+            (button, index) =>
+              !button.isHidden && (
+                <Button
+                  key={index}
+                  variant="primary"
+                  href={button.link}
+                  target="_blank"
+                  style={{ marginLeft: "10px" }}
+                >
+                  {button.icon} &nbsp;
+                  {button.label}
+                </Button>
+              )
+          )}
         </div>
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
