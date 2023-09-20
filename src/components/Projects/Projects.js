85a9995f8311648cd1e975c67540d51ade98c21e
taovc <tao.weijie@epitech.eu>
2023-09-20 14:40:30 +0200

    feat: add new project ```

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,16 +3,11 @@ import { Container, Row, Col } from "react-bootstrap";
 
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import zappy from "@/Assets/Projects/zappy.png";
-import rtype from "@/Assets/Projects/rtype.jpg";
 import area from "@/Assets/Projects/area.png";
 import owl from "@/Assets/Projects/owl.png";
-import dataraise from "@/Assets/Projects/dataraise.png";
 import smartx from "@/Assets/Projects/smartx.png";
 import finance from "@/Assets/Projects/finance.png";
 import { useTranslation } from "react-i18next";
-import { useSpring, animated } from "react-spring";
-import { useInView } from "react-intersection-observer";
 
 function Projects() {
   const { t } = useTranslation();
@@ -22,27 +17,14 @@ function Projects() {
       imgPath:
         "https://res.cloudinary.com/djjxcsdai/image/upload/h_660,w_1080/v1695138129/portfolio/3e.png",
       title: t("Project Leader -- E.Design"),
-      description: t(
-        "Full-stack (Next, Nest, Mongodb, Docker-compose and Cloud (Vps + Nginx)\n\n\
-        Build an e-commerce site allowing customers to buy personalized clothing.\
-        \nIntegration of Stripe, Google and Cloudinary APIs for a better user experience.\
-        \nImplementation of a highly modular backend server respecting REST standards.\n\n\
-        Deployment of test and production versions using technologies such as Docker and Docker Compose.\
-        Deployed on the OVH server with nginx configured."
-      ),
+      description: t("project-1"),
       webLink: "https://www.edesignparis.fr/",
       demoLink: "http://test.e-textile.info/en",
     },
     {
       imgPath: smartx,
       title: t("Fullstack Developer -- Smartx"),
-      description: t(
-        "Full-stack (Angular, Nodejs, Express, Mongodb, Docker-compose)\n\n\
-        Maintaining an administration interface for managing orders, inventory,\
-         and generating personalized invoices using technologies such as Angular and Tailwind CSS.\n\n\
-         The server is based on a microservices architecture with Hapi,\
-          and deployment is automated using to GitLab CI/CD and Docker Compose."
-      ),
+      description: t("project-2"),
       webLink: "https://www.smart-x.io/",
       demoLink: "https://e-shop.someflu.org/#/login?returnUrl=%2Fhome",
     },
@@ -50,19 +32,27 @@ function Projects() {
       imgPath:
         "https://res.cloudinary.com/djjxcsdai/image/upload/h_660,w_1080/v1695208745/portfolio/dataraise_nl08eq.png",
       title: t("DevOps Developer -- Dataraise"),
-      description: t("DevOps (docker-compose, Apache Hadoop with Talent Studio)\n\n\
-      Containerize a back-end service project for OnPremise deployment. Use docker, docker-compose and dockerHub to reduce installation costs.\n\n\
-      Following the first step, approve the service in Cloud mode (AWS)."),
+      description: t("project-3"),
       webLink: "https://www.dataraise.com/",
     },
   ];
   const projects = [
     {
-      imgPath: smartx,
-      title: t("Fullstack Developer -- Smartx"),
-      description: t("Maintain an enterprise order management system..."),
-      demoLink: "https://e-shop.someflu.org/#/login?returnUrl=%2Fhome",
-      webLink: "https://www.smart-x.io/",
+      imgPath: "https://res.cloudinary.com/djjxcsdai/image/upload/h_660,w_1080/v1695213143/portfolio/evogue_hfbdac.png",
+      title: t("Programming teacher -- Evogue"),
+      description: t("project-4"),
+      webLink: "https://www.evogue.fr/",
+    },
+    {
+      imgPath: finance,
+      title: t("Freelance Project -- FinanceAPI"),
+      description: t(
+        "Full-stack (React, Node, Mongodb, Docker-compose and netlify)\n\n\
+        It's an ERP system that supports invoice management and ordering. \n\
+        You can easily import/export excel files to process customer information.\n\n\
+        This system allows you to process customer data more easily and efficiently."
+      ),
+      demoLink: "https://nuvoleviaggi.netlify.app/",
     },
   ];
 
